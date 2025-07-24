e9c5da904265dddaba92c504e7e52b3c2d471fc3
ayala49890 <ayala49890@gmail.com>
2025-07-24 15:51:14 +0300

    add render file

--- server/Portfolio.API/obj/Debug/net8.0/Portfolio.API.AssemblyInfo.cs
+++ server/Portfolio.API/obj/Debug/net8.0/Portfolio.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28922235fc937c42c437865572c5eada9e80832f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e95aadbd672bfd84c2259018f9e40cbe240a748")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
